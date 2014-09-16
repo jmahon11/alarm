9a513785e0f0169046be6c296f7c2ff5ff5af4c7
Dimitris Zlatanidis <d.zlatanidis@gmail.com>
2014-09-16 04:36:41 +0300

    updated for version 1.1

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -27,65 +27,79 @@ import time
 
 __all__ = "alarm"
 __author__ = "dslackw"
-__version_info__ = (1, 0)
+__version_info__ = (1, 1)
 __version__ = "{0}.{1}".format(*__version_info__)
 __license__ = "GNU General Public License v3 (GPLv3)"
 __email__ = "d.zlatanidis@gmail.com"
 
 def start_ALARM(ALARM, SONG):
     '''
-    All the work going on here. To the Authority the right time format
-    and finding the correct path of the file. The Application requires
-    Mplayer to play the alarm sound. Please read which sounds are supported 
-    in page : 
-    http://web.njit.edu/all_topics/Prog_Lang_Docs/html/mplayer/formats.html
+        All the work going on here. To the Authority the 
+        right time format and finding the correct path of 
+        the file. The Application requires Mplayer to play
+        the alarm sound. Please read which sounds are supported 
+        in page : 
+        http://web.njit.edu/all_topics/Prog_Lang_Docs/html/mplayer/formats.html
     '''
+    RUN_ALARM = True
     try:
         ALARM = ALARM.replace(":", " ").split() # split items
         for item in ALARM:
             if len(item) > 2 or len(item) == 1:
                 print("Setting time pattern is 'HH:MM:SS'")
-                sys.exit()
+                RUN_ALARM = False 
         if int(ALARM[0]) in range(0, 24):
             pass
         else:
             print("Error: hour out of range")
-            sys.exit()
+            RUN_ALARM = False
         if int(ALARM[1]) in range(0, 60):
             pass
         else:
             print("Error: minutes out of range")
-            sys.exit()
+            RUN_ALARM = False
         if int(ALARM[2]) in range(0, 60):
             pass
         else:
             print("Error: seconds out of range")
-            sys.exit()
+            RUN_ALARM = False
     except ValueError:
         print("Setting time pattern is 'HH:MM:SS'")
-        sys.exit()
+        RUN_ALARM = False
     if not os.path.isfile(SONG):
         print("Error: the file does not exist")
-        sys.exit()
+        RUN_ALARM = False
     ALARM = ":".join(ALARM) # reset begin format
-    while True:
-        try:
-            os.system("clear")
-            print("+" + "=" * 78 + "+")
-            print("|" + " " * 30 + "CLI Alarm Clock" + " " * 33 + "|")
-            print("+" + "=" * 78 + "+")
-            print("| Alarm set at : %s" % ALARM + " " * (62-len(ALARM)) + "|")
-            print("| Sound file in : %s" % SONG + " " * (61-len(SONG)) + "|")
-            start_time = time.strftime("%H:%M:%S")
-            print("| Time : %s" % start_time+ " " * (70-len(start_time)) + "|")
-            print("+" + "=" * 78 + "+")
-            time.sleep(0.1)
-            if start_time == ALARM:
-                os.system("mplayer '%s'" % SONG)
-                break
-        except KeyboardInterrupt:
-            print("\nAlarm canceled!")
-            break
+    if RUN_ALARM:
+        os.system("clear")
+        print("+" + "=" * 78 + "+")
+        print("|" + " " * 30 + "CLI Alarm Clock" + " " * 33 + "|")
+        print("+" + "=" * 78 + "+")
+        print("| Alarm set at : %s" % ALARM + " " * (62-len(ALARM)) + "|")
+        print("| Sound file in : %s" % SONG + " " * (61-len(SONG)) + "|")
+        print("| Time : " + " " * 70 + "|")
+        print("+" + "=" * 78 + "+")
+        while RUN_ALARM:
+            try:
+                start_time = time.strftime("%H:%M:%S")
+                position(6, 10, start_time)
+                time.sleep(0.1)
+                if start_time == ALARM:
+                    os.system("mplayer '%s'" % SONG)
+                    RUN_ALARM = False
+            except KeyboardInterrupt:
+                print("\nAlarm canceled!")
+                RUN_ALARM = False
+
+def position(x, y, text):
+    '''
+        I dont know how work this but work well.
+        Thanks for that 'rumpel' from :
+        http://stackoverflow.com/questions/7392779/is-it-possible-
+        to-print-a-string-at-a-certain-screen-position-inside-idle
+    '''    
+    sys.stdout.write("\x1b7\x1b[%d;%df%s\x1b8" % (x, y, text))
+    sys.stdout.flush()
 
 def main():
     args = sys.argv
